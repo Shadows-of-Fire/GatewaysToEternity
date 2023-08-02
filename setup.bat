a579c516ea5287fca67f8065f673552990edf120
Brennan Ward <3682588+Shadows-of-Fire@users.noreply.github.com>
2023-08-01 19:40:52 -0700

    buildscript

--- setup.bat
+++
@@ -1,7 +0,0 @@
-call gradlew eclipse --no-daemon
-call gradlew eclipse genEclipseRuns --no-daemon
-for %%I in (.) do set PROJNAME=%%~nxI
-del %PROJNAME%-Client.launch
-del %PROJNAME%-Server.launch
-ren runClient.launch %PROJNAME%-Client.launch
-ren runServer.launch %PROJNAME%-Server.launch
