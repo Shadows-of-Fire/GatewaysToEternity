c024ab4897662675301419bbfa0cf5f008333519
Shadows_of_Fire <3682588+Shadows-of-Fire@users.noreply.github.com>
2025-08-25 03:10:51 -0700

    Update sound attenuation rules for non-default sounds

--- src/main/java/dev/shadowsoffire/gateways/client/GatewayTickableSound.java
+++ src/main/java/dev/shadowsoffire/gateways/client/GatewayTickableSound.java
@@ -1,5 +1,6 @@
 package dev.shadowsoffire.gateways.client;
 
+import dev.shadowsoffire.gateways.GatewayObjects;
 import dev.shadowsoffire.gateways.entity.GatewayEntity;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.resources.sounds.AbstractTickableSoundInstance;
@@ -9,15 +10,22 @@ public class GatewayTickableSound extends AbstractTickableSoundInstance {
     private final GatewayEntity gateway;
 
     public GatewayTickableSound(GatewayEntity gateway) {
-        super(gateway.getGateway().getSoundtrack().value(), SoundSource.HOSTILE, gateway.level().getRandom());
+        super(gateway.getGateway().soundtrack().value(), SoundSource.HOSTILE, gateway.level().getRandom());
         this.gateway = gateway;
         this.looping = true;
         this.delay = 0;
         this.x = (float) gateway.getX();
         this.y = (float) gateway.getY();
         this.z = (float) gateway.getZ();
-        // this.relative = true;
-        this.pitch = 0.75F;
+
+        // Magic special casing for the default sound. TODO: Data-driven sound properties.
+        if (gateway.getGateway().soundtrack() == GatewayObjects.GATE_AMBIENT) {
+            this.pitch = 0.75F;
+            this.attenuation = Attenuation.LINEAR;
+        }
+        else {
+            this.attenuation = Attenuation.NONE;
+        }
     }
 
     @Override
@@ -26,7 +34,7 @@ public class GatewayTickableSound extends AbstractTickableSoundInstance {
             this.stop();
         }
         else {
-            this.volume = 0.35F - (float) (Minecraft.getInstance().player.distanceToSqr(this.gateway) / (18 * 18F)) / 4F;
+            this.volume = (1 - (float) (Minecraft.getInstance().player.distanceTo(this.gateway) / this.gateway.getGateway().rules().leashRange())) * 0.33F;
         }
     }
 
